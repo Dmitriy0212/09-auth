4436b6f355781262407bd00595b2c6c8bee3fb74
Dmitriy0212 <demarr68@gmail.com>
2026-05-31 00:39:47 +0300

    fix

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,17 +1,18 @@
 import css from '../LayoutNotes.module.css';
-import SidebarNotes from './@sidebar/default';
+
 export default function LayoutNotes({
   children,
+  sidebar,
 }: Readonly<{
   children: React.ReactNode;
+  sidebar: React.ReactNode;
 }>) {
   return (
     <div className={css.container}>
       <div className={css.sidebar}>
-        <div className={css.notesWrapper}>
-          <SidebarNotes />
-        </div>
+        <div className={css.notesWrapper}>{sidebar}</div>
       </div>
+
       {children}
     </div>
   );
